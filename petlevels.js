8f96ec125a7b794de6a2bb9894867a43b7eb0825
Cindy <cindw@hotmail.com>
2015-07-15 16:18:21 -0400

    prompt

--- petlevels.js
+++ petlevels.js
@@ -2,7 +2,7 @@ var username = prompt("Enter your name.", "Name");
 var lvl = 1;
 
 
-document.ready(function() {
+$(document).ready(function() {
     if (username != null) {
 	console.log(username);
 	alert("Welcome " + username + "!");
