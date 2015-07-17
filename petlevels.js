f1b1f2747d67fc4a38a9ba6347077eb6c1e1650a
Cindy <cindw@hotmail.com>
2015-07-17 14:15:32 -0400

    test cookie

--- petlevels.js
+++ petlevels.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     user = prompt("Enter your name.", "Name");
     if (user != null) {
@@ -10,10 +8,6 @@ $(document).ready(function() {
 
 console.log('cookie');
 console.log(document.cookie);
-
-var get_cookie = function(cookie) {
-    var clist = document.cookie.split(';');
-};
 
 var set_user = function(user) {
     document.cookie = document.cookie + "user="+user;
