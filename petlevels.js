e233e026de554ba59a0cf49a328905c902345d9d
Cindy <cindw@hotmail.com>
2015-07-17 14:10:43 -0400

    test cookie

--- petlevels.js
+++ petlevels.js
@@ -1,11 +1,35 @@
 var username;
-var lvl = 1;
+var lvl;
+var hp;
+var mp;
+var str;
+var def;
+var spd;
+var mun;
 
 
 $(document).ready(function() {
     username = prompt("Enter your name.", "Name");
     if (username != null) {
-	$("#content").append(username)
+	document.cookie = "username=" +username; 
+	$("#content").append("Hello " + username + "!");
     }
 });
 
+console.log('cookie');
+console.log(document.cookie);
+
+var get_cookie = function(cookie) {
+    var clist = document.cookie.split(';');
+    for i in clist {
+	if ( i == cookie ) {
+	    break;
+    }
+}
+
+var set_user = function(user) {
+    document.cookie = document.cookie + "user="+user;
+}
+
+var inc_lvl = function() {
+    document.cookie
