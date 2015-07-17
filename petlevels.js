855b2df33e148a87a3060b8b807d5d8296d90f54
Cindy <cindw@hotmail.com>
2015-07-17 14:50:45 -0400

    test cookie

--- petlevels.js
+++ petlevels.js
@@ -21,7 +21,9 @@ $(document).ready(function() {
 
 var get_cookie = function(cookie) {
     var clist = document.cookie.split(';');
-    for ( var i = 0; i < clist.size; i++ ){
+    console.log(clist);
+    for ( var i = 0; i < clist.size; i++ )
+	console.log(clist);
 	console.log(clist[i]);
     }
 };
